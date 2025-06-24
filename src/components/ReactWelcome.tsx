2b6d50d3a70a71563bcf7541e45d3e4b013ecf2b
HaHoLuLa <jh610.lee@gmail.com>
2025-06-24 17:28:41 +0900

    수정

--- src/components/ReactWelcome.tsx
+++ src/components/ReactWelcome.tsx
@@ -2,8 +2,8 @@ export default function Welcome() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">안녕하세요</h1>
-      <p className="text-lg text-gray-700 mb-8">오신 것을 환영합니다!</p>
-      <a className="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition" href="/about">
+      <p className="text-lg text-gray-700 mb-8">저는 리액트입니다.</p>
+      <a className="px-6 py-2 bg-cyan-500 text-white rounded hover:bg-cyan-600 transition" href="/about">
         이동하기
       </a>
     </div>
--- src/components/SvelteWelcome.svelte
+++ src/components/SvelteWelcome.svelte
@@ -4,8 +4,8 @@
 
 <div class="flex flex-col items-center justify-center h-screen">
     <h1 class="text-4xl font-bold mb-4">안녕하세요</h1>
-    <p class="text-lg text-gray-700 mb-8">오신 것을 환영합니다!</p>
-    <a class="px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition" href="/about">
+    <p class="text-lg text-gray-700 mb-8">저는 스벨트입니다.</p>
+    <a class="px-6 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition" href="/about">
         이동하기
     </a>
 </div>
